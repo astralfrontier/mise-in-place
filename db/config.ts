6c79e2e23b4fc7a218f4b123c4b13f524174cd6a
Bill Garrett <garrett@astralfrontier.org>
2026-03-26 21:32:46 -0700

    Define a basic food schema

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,36 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Tag = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    description: column.text(),
+  }
+})
+
+const TagImplies = defineTable({
+  columns: {
+    tagid: column.number({ references: () => Tag.columns.id }),
+    impliestagid: column.number({ references: () => Tag.columns.id })
+  }
+})
+
+const Food = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    description: column.text()
+  }
+})
+
+const FoodTags = defineTable({
+  columns: {
+    foodid: column.number({ references: () => Food.columns.id }),
+    tagid: column.number({ references: () => Tag.columns.id })
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Tag, TagImplies, Food, FoodTags }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,21 @@
-import { db } from 'astro:db';
+import { db, Food, FoodTags, Tag, TagImplies } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Tag).values([
+    { id: 1, name: "Wheat", description: "Contains wheat" },
+    { id: 2, name: "Gluten", description: "Contains gluten" }
+  ])
+
+  await db.insert(TagImplies).values([
+    { tagid: 1, impliestagid: 2 }
+  ])
+
+  await db.insert(Food).values([
+    { id: 1, name: "Bread", description: "It's bread" }
+  ])
+
+  await db.insert(FoodTags).values([
+    { foodid: 1, tagid: 1 }
+  ])
 }
